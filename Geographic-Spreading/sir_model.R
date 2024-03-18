47f1858bcf15e8301d1516c13a3f337209fbada3
fultoncarolyn <carolyn.fulton@colorado.edu>
2024-03-18 10:34:52 -0600

    mtg mar 18

---
+++ Geographic-Spreading/sir_model.R
@@ -0,0 +1,55 @@
+library(deSolve)
+
+
+# Define function of model (ODEs) - why lists? other methods?
+sir_model <- function(time, variables, parameters) {
+  with(as.list(c(variables, parameters)), { #trying to understand structure of this line and reading in variables
+    dS <- -beta * I * S
+    dI <-  beta * I * S - gamma * I
+    dR <-  gamma * I
+    return(list(c(dS, dI, dR)))
+  })
+}
+
+# Define params
+parameters_values <- c(
+  beta  = 0.004, # infectious contact rate (/person/day)
+  gamma = 0.5    # recovery rate (/day)
+)
+
+
+# Define init conds
+initial_values <- c(
+  S = 999,  # number of susceptibles at time = 0
+  I =   1,  # number of infectious at time = 0
+  R =   0   # number of recovered (and immune) at time = 0
+)
+
+# time structure using by = or length = for stepsize is there a best way to do this?
+time_values <- seq(0, 10, by = 0.1)
+
+sir_model_solve <- ode(
+  y = initial_values,
+  times = time_values,
+  func = sir_equations,
+  parms = parameters_values 
+)
+
+sir_model_solve <- as.data.frame(sir_model_solve)
+
+
+# Plotting with how do I force the ranges on axis (ex lost 1000)?
+with(sir_model_solve, {
+  # plotting the time series of susceptibles:
+  plot(time, S, type = "l", col = "blue",
+       xlab = "time (days)", ylab = "number of people")
+  # adding the time series of infectious:
+  lines(time, I, col = "red")
+  # adding the time series of recovered:
+  lines(time, R, col = "green")
+})
+
+# adding a legend:
+legend("right", c("susceptibles", "infectious", "recovered"),
+       col = c("blue", "red", "green"), lty = 1, bty = "n")
+
